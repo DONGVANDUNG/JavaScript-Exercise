dab4f0ff900103fb9199fac9a7210059e461ad16
unknown <vandung030701@gmail.com>
2022-03-16 17:28:53 +0700

    Complete App_ManageStudent

--- app_manageStudent/index.js
+++ app_manageStudent/index.js
@@ -14,8 +14,9 @@
  const inputNameStudent = $('.form-student__name');
  const inputClassStudent = $('.form-student__class');
  const inputPointStudent = $('.form-student__gpa');
- let amountStudent = -1;
- let isEditing = 'true'
+ let list = [];
+ let amount = 0;
+ let isEditing = false;
  const appAddStudent = {
      //handle Events
      handerClick: function() {
@@ -56,122 +57,111 @@
                          inputPointStudent.classList.remove('input-error');
                      }
                  })
-                 if (inputNull == 0) {
-                     let listStudentInLocal = that.getStudentLocalStorage();
-                     amountStudent++;
-                     const studentInfor = {
+                 if (inputNull == 0 && isEditing === false) {
+                     const student = {
+                         id: amount,
                          name: inputNameStudent.value,
                          class: inputClassStudent.value,
-                         gpa: inputPointStudent.value,
-                         id: amountStudent
+                         gpa: inputPointStudent.value
                      }
-                     listStudentInLocal.push(studentInfor);
-                     localStorage.setItem('students', JSON.stringify(listStudentInLocal));
+                     list.push(student);
+                     amount++;
                      modal.style.display = 'none';
                      formStudent.style.display = 'none';
-                     isEditing = false;
-
-                     const icon_deleteStudent = $$('.icon-delete');
-                     const icon_editStudent = $$('.icon-edit');
-                     const icon_hideStudent = $$('.icon-hidden');
-                     ///event delete student
                      that.render();
-                     icon_deleteStudent.forEach(function(item) {
-                             let list = that.getStudentLocalStorage();
-                             console.log(item);
-                             $(item).on('click', function() {
-                                 list.forEach(function(student) {
-                                     if (item.id == student.id) {
-                                         list.splice(item.id - 1, 1);
-                                     }
-                                 })
-                             })
-                             JSON.stringify(localStorage.setItem('student', list));
-                         })
-                         //event click to edit student
-                     icon_editStudent.forEach(function(item) {
-                             $(item).on('click', function() {
-                                 console.log('aa')
-                                 formStudent.style.display = 'flex';
-                                 modal.style.display = 'flex';
-                                 const getValue = that.getStudentLocalStorage();
-                                 let studentSelect = {};
-                                 getValue.forEach(function(student) {
-                                     if (student.id == item.id) {
-                                         studentSelect = student;
-                                     }
-                                 })
-                                 inputNameStudent.value = studentSelect.name;
-                                 inputClassStudent.value = studentSelect.class;
-                                 inputPointStudent.value = studentSelect.gpa;
-                                 isEditing = true;
-                             })
-                         })
-                         ///event hidden student
-                     icon_hideStudent.forEach(function(item) {
-                         item.addEventListener('click', function() {
-                             console.log("aa")
-                             const boxDetailInfor = item.parentElement.parentElement.firstElementChild;
-                             if (boxDetailInfor.style.textDecoration == 'line-through') {
-                                 boxDetailInfor.style.textDecoration = 'none';
-                             } else
-                                 boxDetailInfor.style.textDecoration = 'line-through'
-                         })
-                     })
                  }
-                 // change color border when oninput
-                 input.forEach(function(item) {
-                     item.addEventListener('keyup', function(e) {
-                         if (item.value == "") {
-                             inputNull++;
-                             item.classList.add('input-error');
-                         } else {
-                             item.classList.remove('input-error');
-                             inputPointStudent.classList.remove('input-error');
-                         }
+             },
+             //go to back home
+             btnHome.addEventListener('click', function() {
+                 boxStudent.style.display = 'none';
+                 listStudent.style.display = 'none';
+                 gird.style.display = 'flex';
+             })
+         )
+     },
+     ///render list student
+     render: function() {
+         let html = '';
+         html = list.map(function(student) {
+             return `<div class="student-item" data-item='${student.id}'>
+                    <div class="detail-infor-student" >
+                        <p class="student_name">Tên SV: ${student.name}</p>
+                        <p class="student_class"> Lớp: ${student.class}</p>
+                        <p class="student_gpa"> GPA Tích Lũy: ${student.gpa}</p>
+                    </div>
+                    <div class="btn-change">
+                        <i class="fa-solid fa-trash icon icon-delete" ></i>
+                        <i class="fa-solid fa-wrench icon icon-edit"></i>
+                        <i class="fa-solid fa-ban icon icon-hidden"></i>
+                    </div>
+                </div>`
+         });
+         boxStudent.innerHTML = html.join('');
 
-                         // catch keyboard enter
-                         if (e.keyCode == 13) {
-                             btnSave.click();
-                         }
-                     })
+
+         const icon_deleteStudent = $$('.icon-delete');
+         const icon_editStudent = $$('.icon-edit');
+         const icon_hideStudent = $$('.icon-hidden');
+         ///event delete student
+
+         icon_deleteStudent.forEach(function(item) {
+                 item.addEventListener('click', function() {
+                     item.parentElement.parentElement.remove();
                  })
              })
-             //go to back home
-         btnHome.addEventListener('click', function() {
-             boxStudent.style.display = 'none';
-             listStudent.style.display = 'none';
-             gird.style.display = 'flex';
+             //event click to edit student
+         let that = this;
+         icon_editStudent.forEach(function(item) {
+                 item.addEventListener('click', function() {
+                     formStudent.style.display = 'flex';
+                     modal.style.display = 'flex';
+                     const studentSelect = item.parentElement.parentElement;
+                     list.forEach(item => {
+                         console.log(item.id)
+                         if (item.id == studentSelect.dataset.item) {
+                             inputNameStudent.value = item.name;
+                             inputClassStudent.value = item.class;
+                             inputPointStudent.value = item.gpa;
+                             isEditing = true;
+                             btnSave.addEventListener('click', function() {
+                                 item.name = inputNameStudent.value;
+                                 item.class = inputClassStudent.value;
+                                 item.gpa = inputPointStudent.value;
+                                 modal.style.display = 'none';
+                                 formStudent.style.display = 'none';
+                                 that.render();
+                             })
+                         }
+                     });
+
+                 })
+             })
+             ///event hidden student
+         icon_hideStudent.forEach(function(item) {
+                 item.addEventListener('click', function() {
+                     const itemSelect = item.parentElement.parentElement.firstElementChild
+                     itemSelect.style.textDecoration = itemSelect.style.textDecoration == 'line-through' ? 'none' : 'line-through';
+                 })
+             })
+             // change color border when oninput
+         input.forEach(function(item) {
+             item.addEventListener('keyup', function(e) {
+                 if (item.value == "") {
+                     inputNull++;
+                     item.classList.add('input-error');
+                 } else {
+                     item.classList.remove('input-error');
+                     inputPointStudent.classList.remove('input-error');
+                 }
+
+                 // catch keyboard enter
+                 if (e.keyCode == 13) {
+                     btnSave.click();
+                 }
+             })
          })
      },
-     ///get student in localstorage
-     getStudentLocalStorage: function() {
-         let list = localStorage.getItem('students') == null ? [] : JSON.parse(localStorage.getItem('students'));
-         return list;
-     },
-     render: function() {
-         const students = this.getStudentLocalStorage();
-         if (students) {
-             let html = students.map(student => {
-                 let studentInfor = `<div class="student-item">
-      <div class="detail-infor-student">
-          <p class="student_name"> Tên SV: ${student.name}</p>
-          <p class="student_class"> Lớp: ${student.class}</p>
-          <p class="student_gpa"> GPA Tích Lũy: ${student.gpa}</p>
-      </div>
-      <div class="btn-change">
-          <i class="fa-solid fa-trash icon icon-delete" data-icon-delete=${student.id}></i>
-          <i class="fa-solid fa-wrench icon icon-edit" data-icon-edit=${student.id}></i>
-          <i class="fa-solid fa-ban icon icon-hidden" data-icon-hidden=${student.id}></i>
-      </div>
-  </div>`
-                 return studentInfor
-             });
-             boxStudent.innerHTML = html.join('');
-         }
-     },
      start: function() {
-         this.render();
          this.handerClick();
      }
  }
